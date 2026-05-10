0af92bed6373c3f83c6aef466f9c55d89465cb9c
Vika099 <vika.kr201904@gmail.com>
2026-05-10 12:35:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,6 @@ const startScreen = document.querySelector("#start-screen");
 const quizScreen =  document.querySelector("#quiz-screen");
 const resultScreen =  document.querySelector("#result-screen");
 const startBtn =  document.querySelector("#start-btn");
- 
- btn.addEventListener('click',  () => {
-  gtag('event', 'click_start_btn');
-  console.log('Дані відправлено!');
- });
-
- 
 const restartBtn =  document.querySelector("#restart-btn");
 
 let questionIndex = 0;
